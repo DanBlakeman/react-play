394a568e5a12d193a4d8f4b76b05239e8526cf2b
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-07-07 16:08:39 +0100

    retrieves notes from bacon ipsum api

--- js/Note.js
+++ js/Note.js
@@ -12,6 +12,9 @@ var Note = React.createClass({
       transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
     };
   },
+  componentDidMount: function() {
+    $(this.getDOMNode()).draggable();
+  },
   edit: function () {
     this.setState({editing: true});
   },
@@ -70,6 +73,16 @@ var Board = React.createClass({
     this.uniqueId = this.uniqueId || 0;
     return this.uniqueId++;
   },
+  componentWillMount: function() {
+    var self = this;
+    if(this.props.count) {
+      $.getJSON("http://baconipsum.com/api/?type=all-meat&sentences=" + this.props.count + "&start-with-lorem=1&callback=?", function(results) {
+        results[0].split('. ').forEach(function(sentence) {
+          self.add(sentence.substring(0,40));
+        });
+      });
+    }
+  },
   add: function (text) {
     var arr = this.state.notes;
     arr.push({
@@ -102,5 +115,5 @@ var Board = React.createClass({
   }
 });
 
-React.render(<Board count={10}/>,
+React.render(<Board count={50}/>,
     document.getElementById('react-container'));
